c3ce2072be4d6960f25e6b75ffe97ee335b6c616
Philip Tettey <82468726+iAmQuamz@users.noreply.github.com>
2023-06-13 11:45:25 +0000

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-# program prints argc & argv
 
-from sys import argv
 if __name__ == "__main__":
-    if len(argv) == 1:
-        print("0 arguments.")
-    elif len(argv) == 2:
-        print("1 argument:")
-        print(f"1: {argv[1]}")
-    else:
-        print(f"{(len(argv) - 1)} arguments:")
-        j = 1
-        for i in range(len(argv)-1):
-            print(f"{j}: {argv[j]}")
-            j = j + 1
+    from sys import argv
+    i = 1
+    args = len(argv) - 1
+
+    print(f"{args} {'argument' if args == 1 else 'arguments'}", end="")
+    print(f"{'.' if args == 0 else ':'}")
+    while (i <= args):
+        print(f"{i}: {argv[i]}")
+        i += 1
