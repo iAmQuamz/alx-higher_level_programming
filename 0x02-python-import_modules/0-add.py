24c6b68192cc62620a3036f1acf88ee8bb8c2425
Philip Tettey <82468726+iAmQuamz@users.noreply.github.com>
2023-06-13 11:44:03 +0000

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
-# program imports addition function involving 2 ints and prints result of a+b
-
-
-from add_0 import add
 if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
